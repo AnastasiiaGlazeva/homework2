330d9b9020d106572551d96fe8e198103eac11f9
a.glazyeva <a.glazyeva@talenttech.ru>
2022-02-14 11:26:45 +0300

    Доработки 14.02. Домашнее задание «1.2. Отличия от Java: immutability, переменные, типы данных, операторы и приведение типов»

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -4,31 +4,26 @@ import kotlin.random.Random
 
 fun main(){
  moneyTransfer(3600)
-peoples(980796)
-    musicLover(80)
+peoples(1)
+    musicLover(8000)
 }
 
 fun moneyTransfer(amount:Int){
     val commision = amount*75/10000
-    val result = (amount-commision)
-    val resultless0 = result-3500
+    val mincomm = 3500
 
-    if (commision<3500){
-        if(resultless0<0)
-            println(0)
-        else
-            println(result)
-    }
+    if (commision<mincomm)
+        println(mincomm)
     else
-        println(result)
+        println(commision)
     }
 
 fun peoples(likes:Int){
-    val remains1: Int = (likes%10)
+    val remains1: Int = likes%10
     val remains11 = likes%100
     val text: String
 
-        if (remains1 == 1 && remains11 !==11)
+        if (remains1 == 1 && remains11 !== 11)
             text = " человеку"
         else
         text = " людям"
@@ -38,29 +33,22 @@ fun peoples(likes:Int){
 
 fun musicLover(summ: Int){
     val lover: Boolean = Random.nextBoolean()
-    val middiscount = summ-100
-    val maxdiscount = summ*95/100
-    val lovermindisc = summ*99/100
-    val lovermiddisc = middiscount*99/100
-    val lovermaxdisc = maxdiscount*99/100
+    var result = 0
+    val loveresult : Int
 
     println(lover)
 
-    if(lover == true){
-        if(summ<1000)
-            println(lovermindisc)
-        else if (summ>1001 && summ<10000)
-            println(lovermiddisc)
-        else if (summ>10001)
-            println(lovermaxdisc)
-    }
-    else{
     if(summ<1000)
-        println(summ)
+        result = summ
     else if (summ>1001 && summ<10000)
-        println(middiscount)
+        result = summ-100
     else if (summ>10001)
-        println(maxdiscount)
-    }
+        result = summ*95/100
 
+    if(lover) {
+        loveresult = result*99/100
+        println(loveresult)
+    }
+    else
+        println(result)
 }
